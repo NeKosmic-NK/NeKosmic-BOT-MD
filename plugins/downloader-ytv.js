42dd4c1b87dc474667856cda73443606f420db7a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-19 02:44:03 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,29 +1,48 @@
+let limit = 50
+import fs from 'fs'
+import fetch from 'node-fetch'
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-if (!args || !args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*'
-try {
-let { thumbnail, video, title } = await youtubedl(args[0])
-.catch(async () => await youtubedlv2(args[0]))
-let link = await video['360p'].download()
+let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
+if (!args || !args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*`
+conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴠɪᴅᴇᴏ...⏳_*`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝚁𝙴𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝙾𝚁 𝙳𝙴 𝚅𝙸𝙳𝙴𝙾',
+body: 'ʙʏ ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})
+let chat = global.db.data.chats[m.chat]
 const isY = /y(es)/gi.test(args[1])
-const limitedSize = (isPrems || isOwner ? 350 : 200) * 3074
-let isLimit = limitedSize < video['360p'].fileSize
-if (!isY) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
-*🔥 𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}
-*📁 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾:* ${video['360p'].fileSizeH}
-`.trim(), m)
-await conn.sendFile(m.chat, link, title + '.mp3', `
-*🔥 𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}
-*📁 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾:* ${video['360p'].fileSizeH}
-`.trim(), m, null, {
-asDocument: 0
-})
-}catch(e){
-m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁 𝙰𝙻 𝙴𝙽𝚅𝙸𝙰𝚁 𝙴𝙻 𝙰𝚁𝙲𝙷𝙸𝚅𝙾.. 𝙴𝚂𝚃𝙾 𝚂𝙴 𝙿𝚄𝙴𝙳𝙴 𝙳𝙴𝙱𝙴𝚁 𝙰 𝚀𝚄𝙴 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙴𝚂 𝙳𝙴 𝚄𝙽 𝙿𝙴𝚂𝙾 / 𝚃𝙰𝙼𝙰𝙽̃𝙾 𝙶𝚁𝙰𝙽𝙳𝙴*')
-console.log(e)
+const { thumbnail, video: _video, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+const limitedSize = (isPrems || isOwner ? 350 : limit) * 3074
+let video, source, res, link, lastError, isLimit
+for (let i in _video) {
+try {
+video = _video[i]
+isLimit = limitedSize < video.fileSizeH
+if (isLimit) continue
+link = await video.download()
+if (link) res = await fetch(link)
+isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
+if (isLimit) continue
+if (res) source = await res.arrayBuffer()
+if (source instanceof ArrayBuffer) break
+} catch (e) {
+video = source = link = null
+lastError = e
 }}
+if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw '*[❗] 𝙴𝚁𝚁𝙾𝚁: ' + (lastError || '𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾*')
+let _thumb = {}
+try { _thumb = { thumbnail: await (await fetch(thumbnail)).buffer() } }
+catch (e) { }
+await conn.sendFile(m.chat, link, title + '.mp4', `
+*🔥 𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}
+*📁 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾:* ${video.fileSizeH}
+`.trim(), m, false, {
+..._thumb,
+asDocument: chat.useDocument
+})}
 handler.help = ['mp4', 'v'].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
-handler.exp = 0
+handler.exp = 500
 export default handler
