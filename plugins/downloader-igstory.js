6e4bda6d3bb37ebac6a3dd0b7f2bb9f1963b3e9b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-20 18:33:22 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/downloader-igstory.js
@@ -0,0 +1,16 @@
+/* CREDITOS A https://github.com/FG98F */
+import axios from 'axios'
+import hx from 'hxz-api' 
+let handler = async (m, { conn, args, usedPrefix, command, text}) => {
+if (!text) throw `✳️ Ingrese el nombre de usuario de Instagram\n\n📌 Ejemplo : ${usedPrefix + command} auronplay`        
+hx.igstory(text).then(async (result) => {
+for (let i of result.medias) {
+if (i.url.includes("mp4")) {            
+conn.sendFile(m.chat, i.url, 'igstory.mp4', `✅ Hecho`.trim(), m)
+} else {     
+conn.sendFile(m.chat, i.url, '', '', m)
+}}});}
+handler.help = ['igstory <username>']
+handler.tags = ['downloader']
+handler.command = ['igstory', 'ighistoria' ]
+export default handler
