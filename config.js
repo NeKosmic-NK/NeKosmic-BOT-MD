5f306c3f89a66ab0899ec50bb05bd6e0b3ed14ef
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-21 15:45:52 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ import { fileURLToPath } from 'url'
 global.owner = [
   ['5219992095479', '👑 Mystic - Creador 👑', true],
   ['50575008120', '🤖 Mystic - Bot 🤖', true],
-  ['5219996125657'],
   ['17722386341']
 ] // Cambia los numero que quieras
 global.mods = [] 
