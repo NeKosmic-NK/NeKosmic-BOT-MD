45c5e66f4947c8dcaf33de01da26e69a16af2206
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-22 03:34:10 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/fun-verdad.js
+++ plugins/fun-verdad.js
@@ -1,10 +1,9 @@
 let handler  = async (m, { conn }) => {
-conn.reply(m.chat,`*┌────「 VERDAD 」─*\n*“${pickRandom(global.verdad)}”*\n*└────「 Mystic 」─*`, m)
+conn.reply(m.chat,`*┌────「 𝚅𝙴𝚁𝙳𝙰𝙳 」─*\n*“${pickRandom(global.verdad)}”*\n*└────「 𝙼𝚈𝚂𝚃𝙸𝙲 」─*`, m)
 }
 handler.help = ['verdad']
 handler.tags = ['fun']
 handler.command = /^verdad/i
-handler.fail = null
 export default handler
 
 function pickRandom(list) {
