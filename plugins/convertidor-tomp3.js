bc99a7e6ea2f6991235e97c066c4f24fca8e2ac6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-22 03:20:22 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/convertidor-tomp3.js
+++ plugins/convertidor-tomp3.js
@@ -13,5 +13,5 @@ conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, null, { mimetype: 'audio/m
 }
 handler.help = ['tomp3 (reply)']
 handler.tags = ['audio']
-handler.command = /^to(mp3|a(udio)?)|mp3$/i
+handler.command = ['tomp3', 'toaudio', 'mp3']
 export default handler
