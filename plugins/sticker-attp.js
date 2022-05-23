58927adf554e3dda0e19b0d62699a3fd7ce5f2fb
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-22 21:45:20 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,8 +1,7 @@
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-let stiker = await sticker(null, global.API('xteam', '/attp', { file: '', text: teks }), global.packname, global.author)
-if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, { asSticker: true })
+conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
