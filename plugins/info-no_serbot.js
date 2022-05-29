f31cc95ad9050cd25ce9e2922b3a818dd7d76b4d
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-05-29 11:30:07 +0100

    Update info-no_serbot.js

--- plugins/info-no_serbot.js
+++ plugins/info-no_serbot.js
@@ -1,4 +1,4 @@
-/.
+/*
 import fs from 'fs'
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let text = `
@@ -16,4 +16,4 @@ sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})
 }
 handler.command = /^(jadibot|serbot|bots|subbots|getcode)/i
 export default handler
-/.
+*/
