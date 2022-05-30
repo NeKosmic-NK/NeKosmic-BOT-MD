0661a33965e780896478817980e2710502e84367
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-05-30 10:15:13 +0100

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -5,7 +5,7 @@ import { promises } from 'fs'
 import { join } from 'path'
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) => {
 try {
-let vn = './media/menu.mp3'
+let vn = './media2/menu.mp3'
 let pp = './media2/8.jpg'
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
 let { exp, limit, level, role } = global.db.data.users[m.sender]
