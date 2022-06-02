c59af970003ba0e0e64cedddc7815a03373df4a0
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-06-02 19:01:17 +0100

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -6,7 +6,7 @@ let text = `
 conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 - 𝙾𝚆𝙽𝙴𝚁',
-body: 𝐁𝐲: 𝐍𝐞𝐊𝐨𝐬𝐦𝐢𝐜',         
+body: 'BY NeKosmic',         
 previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
 sourceUrl: `https://wa.me/5219992095479`}}})
 const data = global.owner.filter(([id, isCreator]) => id && isCreator)
