4c40a10e2faf335dd85e1503d6886d85bccf235f
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-06-05 16:26:11 +0100

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,7 +4,7 @@ let group = m.chat
 conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-body: 𝐁𝐘 𝐍𝐞𝐊𝐨𝐬𝐦𝐢𝐜',         
+body: '𝙱𝚈 𝙽𝚎𝙺𝚘𝚜𝚖𝚒𝚌 - 𝙱𝙾𝚃',         
 previewType: 0, thumbnail: fs.readFileSync("./media2/4.jpeg"),
 sourceUrl: `https://www.instagram.com/invites/contact/?i=lwv1dph6f3rt&utm_content=ohvhscn`}}})   
 }
