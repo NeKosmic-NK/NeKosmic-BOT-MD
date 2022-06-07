4ac4ef7f18ddeefd03073a143889aeadad08713c
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-06-07 22:21:54 +0100

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -12,6 +12,8 @@ let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 *4.-* https://chat.whatsapp.com/Gm0veATfRP75zmq1W2Ypb3
 
 *5._* https://chat.whatsapp.com/Gm0veATfRP75zmq1W2Ypb3
+*_TEAM BLACK EAGLE_*
+https://chat.whatsapp.com/H9aWccvYaJADCDYcAkLd0O
 `.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
 handler.command = /^linkgc|grupos$/i
 export default handler
