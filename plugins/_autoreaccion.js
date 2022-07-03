61ede6a858df53141f5d1c91bdcb91a0b8cd7352
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-07-03 14:41:03 +0100

    Update _autoreaccion.js

--- plugins/_autoreaccion.js
+++ plugins/_autoreaccion.js
@@ -1,3 +1,4 @@
+/*
 let handler = async (m, { conn }) => {
 let emot = pickRandom(["🎃", "❤", "😘", "😍", "💕", "😎", "🙌", "⭐", "👻", "🔥"])
 conn.sendMessage(m.chat, {
@@ -5,8 +6,9 @@ react: {
 text: emot,
 key: m.key
 }})}
-handler.customPrefix = /(neko|nekosmic - bot|neko - bot|nekosmic-bot)/i
+handler.customPrefix = /(nekosmic|NeKosmic|NeKosmic Bot|NeKosmic-BOT)/i
 handler.command = new RegExp
 export default handler
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]}
+*/
