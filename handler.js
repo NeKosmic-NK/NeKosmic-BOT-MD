c16c16a55b66cf02646851e14904e1d21cbf1708
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-07-04 12:25:28 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -614,7 +614,7 @@ export async function handler(chatUpdate) {
         if (opts['autoread'])
             await this.readMessages([m.key])
         
-        if (!m.fromMem && m.text.match(/(mystic|the mystic - bot|mystic - bot|themystic-bot)/gi)) {
+        if (!m.fromMem && m.text.match(/(NeKosmic|nekosmic|NeKosmic Bot|nekosmic-bot)/gi)) {
         let emot = pickRandom(["🎃", "❤", "😘", "😍", "💕", "😎", "🙌", "⭐", "👻", "🔥"])
         this.sendMessage(m.chat, { react: { text: emot, key: m.key }})}
         function pickRandom(list) { return list[Math.floor(Math.random() * list.length)]}
