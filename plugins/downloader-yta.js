cf01da248458f724a355a2dbf298507949c5fecb
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-07-14 11:53:50 +0100

    Update downloader-yta.js

--- plugins/downloader-yta.js
+++ plugins/downloader-yta.js
@@ -36,4 +36,4 @@ conn.sendFile(m.chat, source, title + '.mp3', null, m, false, { mimetype: 'audio
 handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
-export default handle
+export default handler
